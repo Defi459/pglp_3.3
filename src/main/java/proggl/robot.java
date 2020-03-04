1d4912b706908473019968591b09102869327a2a
Edouard Longueville <edouard.longueville@ens.uvsq.fr>
2020-03-04 12:36:46 +0100

    Methode avanceTous sans LSP

---
+++ src/main/java/proggl/robot.java
@@ -0,0 +1,10 @@
+package proggl;
+
+public class robot {
+    private String position;
+    private String direction;
+
+    public void tourne(){ /*tourne d'1/4 de tour*/}
+    public void avance(){ /*avance d'une case*/}
+    public void avancerTous(){ /*Appelle avance() pour tous les robots*/}
+}
---
+++ src/main/java/proggl/robotstatique.java
@@ -0,0 +1,8 @@
+package proggl;
+
+public class robotstatique extends robot{
+    @Override
+    public void avance(){/*N'avance pas*/}
+
+
+}
